da8676b1009cf273dccf9a19caa3759166a8d51d
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-08-20 14:31:33 +1000

    Center the robot based of the board cell

--- src/robot/Component.js
+++ src/robot/Component.js
@@ -1,11 +1,16 @@
 import styles from './Component.css';
 import React, { Component } from 'react';
+import { surfaceArea } from '../board/cell/Component.css';
 
 export default ({position}) => {
     const { x, y } = position;
 
     return (
-        <div className={styles.default}>
+        <div className={styles.default}
+            style={{
+            margin: `calc( (${surfaceArea})/2 - ${styles.dimension}/2)`,
+            left: `calc(${surfaceArea} * ${x})`,
+            top: `calc(${surfaceArea} * ${y})`}}>
             Robot
         </div>
     );
