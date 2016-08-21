dd8017d1841cddb824c007ef0197cf99150f6812
Alexander Zaharakis <alex.zaharakis@rea-group.com>
2016-08-21 15:38:06 +1000

    Create a components directory

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 import { AppContainer } from 'react-hot-loader';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './components/app';
 
 const rootEl = document.getElementById('root');
 ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  rootEl
+    <AppContainer>
+        <App />
+    </AppContainer>,
+    rootEl
 );
 
 if (module.hot) {
-  module.hot.accept('./app', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./app').default;
-    ReactDOM.render(
-      <AppContainer>
-         <NextApp />
-      </AppContainer>,
-      rootEl
-    );
-  });
+    module.hot.accept('./components/app', () => {
+        // If you use Webpack 2 in ES modules mode, you can
+        // use <App /> here rather than require() a <NextApp />.
+        const NextApp = require('./components/app').default;
+        ReactDOM.render(
+            <AppContainer>
+                <NextApp />
+            </AppContainer>,
+            rootEl
+        );
+    });
 }
